21121e99c0cb6e5e82d2d1425b971b0a594f66d7
Reto Weiss <reto.weiss@axonivy.com>
2013-10-22 15:36:13 +0000

    * User Story #24203: Techn: Build Designer with maven tycho - Fix test Reviewed by nobody

--- src/test/java/ch/ivyteam/maven/TestSetMavenAndEclipseVersion.java
+++ src/test/java/ch/ivyteam/maven/TestSetMavenAndEclipseVersion.java
@@ -9,6 +9,7 @@ import mockit.MockUp;
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
+import org.codehaus.plexus.util.StringUtils;
 import org.fest.assertions.Assertions;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +17,8 @@ import org.junit.Test;
 
 public class TestSetMavenAndEclipseVersion extends Assertions
 {
+  private static final File POM_FILE = new File("testProject/pom.xml");
+
   private SetMavenAndEclipseVersion testee = new SetMavenAndEclipseVersion();
   
   private StringBuilder log = new StringBuilder();
@@ -23,7 +26,7 @@ public class TestSetMavenAndEclipseVersion extends Assertions
   @Before
   public void before() throws IOException
   {
-    testee.setPomFile(new File("testProject/pom.xml"));
+    testee.setPomFile(POM_FILE);
     testee.setLog(new MockUp<Log>(){
       @Mock void info( CharSequence content )
       {
@@ -70,7 +73,7 @@ public class TestSetMavenAndEclipseVersion extends Assertions
 
   private void comparePom() throws IOException
   {
-    String testeeManifest = FileUtils.readFileToString(new File("testProject/pom.xml"));
+    String testeeManifest = FileUtils.readFileToString(POM_FILE);
     String referenceManifest = FileUtils.readFileToString(new File("referenceProject/pom.xml"));
     assertThat(testeeManifest).isEqualTo(referenceManifest);
   }
@@ -85,6 +88,7 @@ public class TestSetMavenAndEclipseVersion extends Assertions
   private void compareLog() throws IOException
   {
     String referenceLog = FileUtils.readFileToString(new File("referenceProject/log.txt"));
+    referenceLog = StringUtils.replace(referenceLog, "C:\\dev\\maven-plugin\\maven-plugin\\testProject\\", POM_FILE.getParentFile().getCanonicalPath()+"\\");
     assertThat(log.toString()).isEqualTo(referenceLog);
   }
 }
