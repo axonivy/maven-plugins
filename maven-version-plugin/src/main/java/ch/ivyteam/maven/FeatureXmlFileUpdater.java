6a2868ed86a4cdac6347d504463c4990c0e7e1ea
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2014-09-17 09:24:53 +0000

    move to sub-project location Reviewed by NOBODY

---
+++ maven-version-plugin/src/main/java/ch/ivyteam/maven/FeatureXmlFileUpdater.java
@@ -0,0 +1,129 @@
+package ch.ivyteam.maven;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.maven.plugin.logging.Log;
+import org.w3c.dom.Node;
+
+/**
+ * Updates the version of the feature and the included features and plugins 
+ */
+class FeatureXmlFileUpdater extends AbstractXmlFileUpdater
+{
+  private File projectDirectory;
+  private String featureVersion;
+  private String requiredVersion;
+  private Log log;
+
+  FeatureXmlFileUpdater(File projectDirectory, String newVersion, Log log)
+  {
+    super(new File(projectDirectory, "feature.xml"));
+    this.projectDirectory = projectDirectory;
+    if (newVersion.indexOf('-') >= 0)
+    {
+      featureVersion = StringUtils.substringBefore(newVersion, "-");
+    }
+    else
+    {
+      featureVersion = newVersion;
+    }
+    requiredVersion = featureVersion;
+    featureVersion += ".qualifier";    
+    this.log = log;
+  }
+
+  void update() throws IOException, ParserConfigurationException, Exception
+  {
+    boolean changed = false;
+    if (xmlFile.exists())
+    {
+      readXml();
+      changed = updateVersion(changed);
+      changed = updateIncludesVersion(changed);
+      changed = updatePluginsVersion(changed);
+      if (changed)
+      {
+        saveXml();
+      }
+      else
+      {
+        log.info("Feature file "+xmlFile.getAbsolutePath()+" is up to date. Nothing to do.");
+      }
+    }
+    else
+    {
+      log.info("No feature.xml found in project "+projectDirectory+". Nothing to do");
+    }
+  }
+
+  private boolean updatePluginsVersion(boolean changed) throws XPathExpressionException
+  {
+    String xPath = "/feature/plugin";
+    List<Node> pluginNodes = findNodes(xPath);
+    for (Node pluginNode : pluginNodes)
+    {
+      Node versionNode = getVersionAttributeNode(pluginNode);
+      if (versionNeedsUpdate(pluginNode, versionNode, requiredVersion))
+      {
+        replaceAttributeText(pluginNode, versionNode, requiredVersion);
+        log.info("Replace version "+versionNode.getTextContent()+" with version "+requiredVersion+" in plugin node "+getAttributeText(pluginNode, "id")+" of feature file "+xmlFile.getAbsolutePath());
+        changed = true;       
+      }
+    }
+    return changed;
+  }
+
+  private boolean updateIncludesVersion(boolean changed) throws XPathExpressionException
+  {
+    String xPath = "/feature/includes";
+    List<Node> includesNodes = findNodes(xPath);
+    for (Node includesNode : includesNodes)
+    {
+      Node versionNode = getVersionAttributeNode(includesNode);
+      if (versionNeedsUpdate(includesNode, versionNode, requiredVersion))
+      {
+        replaceAttributeText(includesNode, versionNode, requiredVersion);
+        log.info("Replace version "+versionNode.getTextContent()+" with version "+requiredVersion+" in includes node "+getAttributeText(includesNode, "id")+" of file feature "+xmlFile.getAbsolutePath());
+        changed = true;       
+      }
+    }
+    return changed;
+  }
+
+  private boolean updateVersion(boolean changed) throws Exception
+  {
+    String xPath = "/feature";
+    Node featureNode = findNode(xPath);
+    Node versionNode = getVersionAttributeNode(featureNode);
+    if (versionNeedsUpdate(versionNode, featureVersion))
+    {
+      replaceAttributeText(featureNode, versionNode, featureVersion);
+      log.info("Replace feature version "+versionNode.getTextContent()+" with version "+featureVersion+" in feature file "+xmlFile.getAbsolutePath());
+      return true;      
+    }
+    return changed;
+  }
+
+  private boolean versionNeedsUpdate(Node parentNode, Node versionNode, String version)
+  {
+    String id = getAttributeText(parentNode, "id");
+    return IvyArtifactDetector.isLocallyBuildIvyArtifact(id, version) &&
+           versionNeedsUpdate(versionNode, version);
+  }
+  
+  private String getAttributeText(Node elementNode, String attribute)
+  {
+    return elementNode.getAttributes().getNamedItem(attribute).getTextContent();
+  }
+
+  private Node getVersionAttributeNode(Node node)
+  {
+    return node.getAttributes().getNamedItem("version");
+  }
+}
---
+++ maven-version-plugin/src/main/java/ch/ivyteam/maven/PomXmlFileUpdater.java
@@ -0,0 +1,170 @@
+package ch.ivyteam.maven;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.xpath.XPathExpressionException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.maven.plugin.logging.Log;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+/** 
+ * Updates the version in maven pom files
+ */
+class PomXmlFileUpdater extends AbstractXmlFileUpdater
+{
+  private String version;
+  private Log log;
+  private String bundleVersion;
+
+  public PomXmlFileUpdater(File xmlFile, String version, Log log)
+  {
+    super(xmlFile);
+    this.version = version;
+    if (version.indexOf('-') >= 0)
+    {
+      bundleVersion = StringUtils.substringBefore(version, "-");
+    }
+    else
+    {
+      bundleVersion = version;
+    }
+
+    this.log = log;
+  }
+
+  void update() throws SAXException, IOException, XPathExpressionException
+  {
+    boolean changed = false;
+    readXml();
+
+    changed = updateVersion(changed);
+    changed = updateParentVersion(changed);
+    changed = updateDependenciesVersion(changed);
+    changed = updateDependenciesVersionInTychoSurefirePlugin(changed);
+
+    if (changed)
+    {
+      saveXml();
+    }
+    else
+    {
+      log.info("Pom file "+xmlFile.getAbsolutePath()+" is up to date. Nothing to do.");
+    }
+  }
+
+  private boolean updateVersion(boolean changed) throws XPathExpressionException
+  {
+    return updateVersion(changed, "/project/version");
+  }
+  
+  private boolean updateParentVersion(boolean changed) throws XPathExpressionException
+  {
+    return updateVersion(changed, "/project/parent/version");
+  }
+
+  private boolean updateDependenciesVersion(boolean changed) throws XPathExpressionException
+  {
+    for (Node dependency : findNodes("/project/dependencies/dependency"))
+    {
+      if (dependencyVersionNeedsUpdate(dependency))
+      {
+        updateDependencyVersion(dependency);
+        changed = true;
+      }
+    }
+    return changed;
+  }
+
+  private boolean updateDependenciesVersionInTychoSurefirePlugin(boolean changed) throws XPathExpressionException
+  {
+    for (Node plugin : findNodes("/project/build/plugins/plugin"))
+    {
+      if (isTychoSurefirePlugin(plugin))
+      {
+        for (Node dependency : getDependencyNodes(plugin))
+        {
+          if (pluginDependencyVersionNeedsUpdate(dependency))
+          {
+            updatePluginDependencyVersion(dependency);
+            changed = true;
+          }
+        }
+      }
+    }
+    return changed;
+  }
+  
+  private boolean pluginDependencyVersionNeedsUpdate(Node dependency)
+  {
+    return isIvyArtifact(dependency) && versionNodeNeedsUpdate(dependency);
+  }
+
+  private boolean dependencyVersionNeedsUpdate(Node dependency)
+  {
+    return isXpertIvyGroupAndIvyArtifact(dependency)&&versionNodeNeedsUpdate(dependency);
+  }
+
+  private boolean isXpertIvyGroupAndIvyArtifact(Node dependency)
+  {
+    return getChildNodeText(dependency, "groupId").equals("Xpert.ivy") && isIvyArtifact(dependency);
+  }
+
+  private boolean isIvyArtifact(Node dependency)
+  {
+    String artifactId = getChildNodeText(dependency, "artifactId");
+    return IvyArtifactDetector.isLocallyBuildIvyArtifact(artifactId, bundleVersion);
+  }
+
+  private boolean versionNodeNeedsUpdate(Node dependency)
+  {
+    Node versionNode = getChildNode(dependency, "version");
+    return versionNeedsUpdate(versionNode, version);
+  }
+
+  private List<Node> getDependencyNodes(Node plugin)
+  {
+    Node configurationNode = getChildNode(plugin, "configuration");
+    Node dependenciesNode = getChildNode(configurationNode, "dependencies");
+    return getChildNodes(dependenciesNode, "dependency");
+  }
+
+  private boolean isTychoSurefirePlugin(Node plugin)
+  {
+    return getChildNodeText(plugin, "groupId").equals("org.eclipse.tycho") && 
+           getChildNodeText(plugin, "artifactId").equals("tycho-surefire-plugin");
+  }
+
+  private void updateDependencyVersion(Node dependency)
+  {
+    Node versionNode = getChildNode(dependency, "version");
+    replaceElementText(versionNode, version);
+    log.info("Replace version "+versionNode.getTextContent()+" with version "+version+" in dependency node of artifact "+getChildNodeText(dependency, "artifactId")+" of pom file "+xmlFile.getAbsolutePath());
+  }
+  
+  private void updatePluginDependencyVersion(Node dependency)
+  {
+    Node versionNode = getChildNode(dependency, "version");
+    replaceElementText(versionNode, bundleVersion);
+    log.info("Replace version "+versionNode.getTextContent()+" with version "+bundleVersion+" in tycho-surefire-plugin dependency node of artifact "+getChildNodeText(dependency, "artifactId")+" of pom file "+xmlFile.getAbsolutePath());
+  }
+
+
+  private boolean updateVersion(boolean changed, String xPathStr)
+          throws XPathExpressionException
+  {
+    Node versionNode = findNode(xPathStr);
+    if (versionNeedsUpdate(versionNode, version))
+    {
+      replaceElementText(versionNode, version);
+      log.info("Replace version "+versionNode.getTextContent()+" with version "+version+" in node "+xPathStr+" of pom file "+xmlFile.getAbsolutePath());
+      changed = true;
+    }
+    return changed;
+  }
+
+
+}
