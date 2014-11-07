1f6b2759af5e1fe39c6d75421bdad879e9b865df
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2014-11-07 15:49:21 +0000

    Story #25208 Improve configuration of ivy maven project-build-plugin - test compilation:   * update engine to compile once a day.   * guarantee that project to build has not pre-built designer artifacts. Reviewed by NOBODY

--- project-build-plugin/src/test/java/ch/ivyteam/ivy/maven/TestCompileProjectMojo.java
+++ project-build-plugin/src/test/java/ch/ivyteam/ivy/maven/TestCompileProjectMojo.java
@@ -3,7 +3,11 @@ package ch.ivyteam.ivy.maven;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -15,16 +19,18 @@ import org.apache.maven.artifact.repository.DefaultArtifactRepositoryFactory;
 import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
 import org.apache.maven.plugin.LegacySupport;
 import org.apache.maven.plugin.internal.DefaultLegacySupport;
-import org.junit.Ignore;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import ch.ivyteam.ivy.maven.log.LogCollector;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestCompileProjectMojo
 {
   private static final String LOCAL_REPOSITORY = getLocalRepoPath();
-  private static final String CACHE_DIR = LOCAL_REPOSITORY + "/.cache/ivy";
+  private static final String CACHE_DIR = LOCAL_REPOSITORY + "/.cache/ivy-dev";
   
   private static String getLocalRepoPath()
   {
@@ -40,6 +46,89 @@ public class TestCompileProjectMojo
     return defaultHomePath.toString();
   }
   
+  /**
+   * Test must run before any other test that invokes the ivyEngine (as it will bind the logger to assert).
+   * @throws Exception
+   */
+  @Test
+  public void A_logging() throws Exception
+  {
+    LogCollector logCollector = new LogCollector();
+    CompileProjectMojo mojo = rule.getMojo();
+    mojo.setLog(logCollector);
+    mojo.execute();
+    assertThat(logCollector.getLogs())
+      .as("Logs from engine must be forwarded")
+      .isNotEmpty();
+    
+    assertThat(logCollector.getWarnings()).isEmpty();
+    assertThat(logCollector.getErrors()).isEmpty();
+  }
+
+  @Test
+  public void B_buildWithExistingProject() throws Exception
+  {
+    CompileProjectMojo mojo = rule.getMojo();
+    
+    File dataClassDir = new File(mojo.project.getBasedir(), "src_dataClasses");
+    File wsProcDir = new File(mojo.project.getBasedir(), "src_wsproc");
+    File classDir = new File(mojo.project.getBasedir(), "classes");
+    FileUtils.cleanDirectory(wsProcDir);
+    FileUtils.cleanDirectory(dataClassDir);
+    FileUtils.cleanDirectory(classDir);
+    
+    mojo.buildApplicationDirectory = Files.createTempDirectory("MyBuildApplication").toFile();
+    mojo.execute();
+    
+    assertThat(findFiles(dataClassDir, "java")).hasSize(2);
+    assertThat(findFiles(wsProcDir, "java")).hasSize(1);
+    assertThat(findFiles(classDir, "class")).hasSize(5);
+  }
+  
+  private static Collection<File> findFiles(File dir, String fileExtension)
+  {
+    if (!dir.exists())
+    {
+      return Collections.emptyList();
+    }
+    return FileUtils.listFiles(dir, new String[]{fileExtension}, true);
+  }
+
+  @Rule
+  public ProjectMojoRule<EnsureInstalledEngineMojo> installUpToDateEngineRule = new ProjectMojoRule<EnsureInstalledEngineMojo>(
+          new File("src/test/resources/base"), EnsureInstalledEngineMojo.GOAL){
+    @Override
+    protected void before() throws Throwable {
+      super.before();
+      
+      getMojo().engineListPageUrl = new URL("http://zugprobldmas/job/Trunk_All/lastSuccessfulBuild/");
+      getMojo().engineCacheDirectory = new File(CACHE_DIR);
+      deleteOutdatedEngine();
+      getMojo().execute();
+    }
+  
+    private void deleteOutdatedEngine() throws IOException
+    {
+      File engineDir = getMojo().getEngineDirectory();
+      getMojo().engineDirectory = engineDir;
+      if (!engineDir.exists())
+      {
+        return;
+      }
+      File releaseNotes = new File(engineDir, "ReleaseNotes.txt");
+      BasicFileAttributes attr = Files.readAttributes(releaseNotes.toPath(), BasicFileAttributes.class);
+      Calendar cal = Calendar.getInstance();
+      cal.add(Calendar.DAY_OF_YEAR, -1);
+      long yesterday = cal.getTimeInMillis();
+      boolean cachedEngineIsOlderThan24h = yesterday > attr.creationTime().toMillis();
+      if (cachedEngineIsOlderThan24h)
+      {
+        System.out.println("Deleting cached outdated engine.");
+        FileUtils.deleteDirectory(engineDir);
+      }
+    }
+  };
+  
   @Rule
   public ProjectMojoRule<CompileProjectMojo> rule = new ProjectMojoRule<CompileProjectMojo>(
           new File("src/test/resources/base"), CompileProjectMojo.GOAL){
@@ -48,8 +137,9 @@ public class TestCompileProjectMojo
       super.before();
       
       getMojo().localRepository = provideLocalRepository();
+      getMojo().engineCacheDirectory = new File(CACHE_DIR);
     }
-
+  
     /**
      * maven-plugin-testing-harness can not inject local repositories (though the real runtime supports it).
      * and the default stubs have no sufficient implementation of getPath(): 
@@ -71,54 +161,5 @@ public class TestCompileProjectMojo
       return localRepository;
     }
   };
-  
-  @Test
-  @Ignore("needs unpacked engine and is more like an integration test...")
-  public void buildWithExistingProject() throws Exception
-  {
-    CompileProjectMojo mojo = rule.getMojo();
-    
-    File dataClassDir = new File(mojo.project.getBasedir(), "src_dataClasses");
-    File wsProcDir = new File(mojo.project.getBasedir(), "src_wsproc");
-    File classDir = new File(mojo.project.getBasedir(), "classes");
-    
-    assertThat(findFiles(wsProcDir, "java")).isEmpty();
-    assertThat(findFiles(dataClassDir, "java")).isEmpty();
-    assertThat(findFiles(classDir, "class")).isEmpty();
-    
-    mojo.buildApplicationDirectory = Files.createTempDirectory("MyBuildApplication").toFile();
-    mojo.engineCacheDirectory = new File(CACHE_DIR);
-    mojo.execute();
-    
-    assertThat(findFiles(dataClassDir, "java")).hasSize(2);
-    assertThat(findFiles(wsProcDir, "java")).hasSize(1);
-    assertThat(findFiles(classDir, "class")).hasSize(5);
-  }
-  
-  @Test
-  public void testLogging() throws Exception
-  {
-    LogCollector logCollector = new LogCollector();
-    CompileProjectMojo mojo = rule.getMojo();
-    mojo.setLog(logCollector);
-    mojo.engineCacheDirectory = new File(CACHE_DIR);
-    mojo.execute();
-    assertThat(logCollector.getLogs())
-      .as("Logs from engine must be forwarded")
-      .isNotEmpty();
-    
-    System.out.println(logCollector.getWarnings().get(0));
-    assertThat(logCollector.getWarnings()).isEmpty();
-    assertThat(logCollector.getErrors()).isEmpty();
-  }
-  
-  private static Collection<File> findFiles(File dir, String fileExtension)
-  {
-    if (!dir.exists())
-    {
-      return Collections.emptyList();
-    }
-    return FileUtils.listFiles(dir, new String[]{fileExtension}, true);
-  }
 
 }
