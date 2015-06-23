97b4bb16cdb52b76d80a3397e85093033aa64e2f
Reto Weiss <reto.weiss@axonivy.com>
2015-06-23 14:04:27 +0000

    User Story #25541: [Techn] Maven mojo for generating system database meta depending java source, sql and documentation files - Copy tests of ch.ivyteam.db.meta.tests to maven plugin db-meta tests Reviewed by nobody

---
+++ db-meta-plugin/src/test/java/test/ch/ivyteam/db/meta/generator/internal/TestIssue23610.java
@@ -0,0 +1,26 @@
+package ch.ivyteam.db.meta.generator.internal;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+/**
+ * Regression test for Issue #23610
+ * @author rwei
+ * @since 29.06.2012
+ */
+public class TestIssue23610
+{
+  /**
+   * Regression test for ISSUE 23610
+   * 
+   * IMPORTANT: 
+   * Not dropping unique constraints before altering table may not break system database conversion test. Also manually test may work.
+   * But there are some SQL Server databases out there which cannot be converted when not dropping unique constraints! See Issue 23610 for details. 
+   */
+  @Test
+  public void testDropUniqueConstraintsBeforeAlterColumn()
+  {
+    assertThat(new MsSqlServerSqlScriptGenerator().isRecreationOfUniqueConstraintsOnAlterTableNeeded()).isTrue();
+  }
+}
