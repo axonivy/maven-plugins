a398364bf4e278d7b6ea9846728b49c41a3af506
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2016-03-08 15:17:17 +0000

    XIVY-756 Release Axon.ivy 6.1 - fix compiler error Reviewed by Jenkins

--- thirdparty-dependency-readme-plugin/src/main/java/ch/ivyteam/ivy/generator/LibraryEntry.java
+++ thirdparty-dependency-readme-plugin/src/main/java/ch/ivyteam/ivy/generator/LibraryEntry.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import ch.ivyteam.ivy.generator.MavenCentral.CentralResponse;
 import ch.ivyteam.xml.XmlUtil;
