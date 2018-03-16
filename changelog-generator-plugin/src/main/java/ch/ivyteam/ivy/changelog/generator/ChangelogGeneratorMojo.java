1a5f67a09784494c4dcaf3ea94deb7aeb920fe99
Alexander Suter <alexander.suter@axonivy.com>
2018-03-16 17:48:16 +0100

    XIVY-2240 Merge Release Notes with N&N - Use fileset instead of custom dir

--- changelog-generator-plugin/src/main/java/ch/ivyteam/ivy/changelog/generator/ChangelogGeneratorMojo.java
+++ changelog-generator-plugin/src/main/java/ch/ivyteam/ivy/changelog/generator/ChangelogGeneratorMojo.java
@@ -1,7 +1,7 @@
 package ch.ivyteam.ivy.changelog.generator;
 
 import java.io.File;
-import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -15,14 +15,16 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 import org.apache.maven.settings.Server;
+import org.apache.maven.shared.model.fileset.FileSet;
+import org.apache.maven.shared.model.fileset.util.FileSetManager;
 
 import ch.ivyteam.ivy.changelog.generator.format.AsciiFormatter;
 import ch.ivyteam.ivy.changelog.generator.format.Formatter;
 import ch.ivyteam.ivy.changelog.generator.format.MarkdownFormatter;
 import ch.ivyteam.ivy.changelog.generator.jira.JiraResponse.Issue;
 import ch.ivyteam.ivy.changelog.generator.jira.JiraService;
-import ch.ivyteam.ivy.changelog.generator.util.FileUtil;
 import ch.ivyteam.ivy.changelog.generator.util.TokenReplacer;
 
 @Mojo(name = "generate-changelog", requiresProject = false)
@@ -50,14 +52,13 @@ public class ChangelogGeneratorMojo extends AbstractMojo
   /** comma separated list of jira projects for example: XIVY, IVYPORTAL */
   @Parameter(property = "jiraProjects", defaultValue = "XIVY")
   public String jiraProjects;
-
   
-  @Parameter(property = "dir", required = true)
-  /** lesen schreiben todo */
-  public String dir; // todo fileset FileSet / in/out
+  /** files which tokens must be replaced */
+  @Parameter(property="fileset", required = true)
+  public FileSet fileset;
   
-  
-  
+  @Parameter(property = "project", required = false, readonly = true)
+  MavenProject project;
   @Parameter(property = "session", required = true, readonly = true)
   MavenSession session;
 
@@ -68,15 +69,14 @@ public class ChangelogGeneratorMojo extends AbstractMojo
 
     List<Issue> issues = loadIssuesFromJira();
 
-    for (Path file : FileUtil.getAllFiles(dir))
+    for (File file : getAllFiles())
     {
-      File f = file.toFile();
-      Formatter formatter = findFormatter(f);
+      Formatter formatter = findFormatter(file);
       if (formatter != null)
       {
-        getLog().info("replace tokens in " + f.getAbsolutePath());
+        getLog().info("replace tokens in " + file.getAbsolutePath());
         Map<String, String> tokens = generateTokens(issues, formatter);
-        new TokenReplacer(f, tokens).replaceTokens();
+        new TokenReplacer(file, tokens).replaceTokens();
       }
     }
   }
@@ -93,7 +93,14 @@ public class ChangelogGeneratorMojo extends AbstractMojo
       throw new MojoExecutionException("could not load issues from jira", ex);
     }
   }
-
+  
+  private List<File> getAllFiles()
+  {
+    return Arrays.stream(new FileSetManager().getIncludedFiles(fileset))
+            .map(f -> new File(fileset.getDirectory() + File.separatorChar + f))
+            .collect(Collectors.toList());
+  }
+  
   private static Formatter findFormatter(File file)
   {
     return formatters.stream()
@@ -101,7 +108,7 @@ public class ChangelogGeneratorMojo extends AbstractMojo
             .findFirst()
             .orElse(null);
   }
-  
+
   private static Map<String, String> generateTokens(List<Issue> issues, Formatter formatter)
   {
     Map<String, String> tokens = new HashMap<>();
