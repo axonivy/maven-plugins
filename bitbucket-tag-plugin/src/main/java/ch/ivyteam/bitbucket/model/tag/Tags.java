0846af097abb6fb71c5021719c23a44c17f7cab7
Reto Weiss <reto.weiss@axonivy.com>
2020-03-30 09:09:20 +0200

    Get all issues from all repositories since a certain release

--- bitbucket-tag-plugin/src/main/java/ch/ivyteam/bitbucket/model/tag/Tags.java
+++ bitbucket-tag-plugin/src/main/java/ch/ivyteam/bitbucket/model/tag/Tags.java
@@ -1,10 +1,10 @@
-package ch.ivyteam.bitbucket.model.commit;
+package ch.ivyteam.bitbucket.model.tag;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import ch.ivyteam.bitbucket.model.paged.PagedResult;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Commits extends PagedResult<Commit>
+public class Tags extends PagedResult<Tag>
 {
 }
