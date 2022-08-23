f120a0373afac0c36511355bb5b86514002c79cf
Alexander Suter <alexander.suter@axonivy.com>
2022-08-23 22:52:10 +0200

    Fix jflex compile errors in ParserGeneratorMojo

--- parser-generator-plugin/src/main/java/ch/ivyteam/parser/maven/ParserGeneratorMojo.java
+++ parser-generator-plugin/src/main/java/ch/ivyteam/parser/maven/ParserGeneratorMojo.java
@@ -8,9 +8,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import java_cup.internal_error;
-import jflex.Options;
-
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -23,30 +20,34 @@ import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.DirectoryScanner;
 import org.sonatype.plexus.build.incremental.BuildContext;
 
+import java_cup.internal_error;
+import jflex.exceptions.SilentExit;
+import jflex.option.Options;
+
 @Mojo(name="generate-parser", defaultPhase=LifecyclePhase.GENERATE_SOURCES)
 public class ParserGeneratorMojo extends AbstractMojo
-{  
+{
   static final String GOAL = "generate-parser";
 
   @Parameter(required = true)
   private File parserFile;
-  
+
   @Parameter(required = true)
   private File scannerFile;
-  
+
   @Parameter(defaultValue="${project.build.directory}/generated-sources/parser", required=true)
   private File outputDirectory;
-    
+
   @Component
   private BuildContext buildContext;
-  
+
   @Parameter(defaultValue="${project}", required=true, readonly=true)
   MavenProject project;
 
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException
   {
-    try 
+    try
     {
       boolean generated = false;
       File packageDir = parsePackageDir();
@@ -78,7 +79,7 @@ public class ParserGeneratorMojo extends AbstractMojo
     File javaParser = new File(packageDir, "Parser.java");
     return fileUpToDate(parserFile, javaParser);
   }
-  
+
   private boolean scannerUpToDate(File packageDir)
   {
     File javaScanner = new File(packageDir, "Scanner.java");
@@ -153,7 +154,7 @@ public class ParserGeneratorMojo extends AbstractMojo
     getLog().info("Generating parser to "+packageDir+ "...");
 
     List<String> args = new ArrayList<String>();
-    args.add("-destdir");    
+    args.add("-destdir");
     args.add(packageDir.getAbsolutePath());
     args.add("-interface");
     args.add("-parser");
@@ -161,15 +162,15 @@ public class ParserGeneratorMojo extends AbstractMojo
     args.add("-symbols");
     args.add("Symbols");
     args.add(parserFile.getAbsolutePath());
-    
+
     java_cup.Main.main(args.toArray(new String[args.size()]));
-    
+
     getLog().info("Parser generated to "+ packageDir + ".");
     return true;
 
   }
 
-  private boolean generateScanner(File packageDir)
+  private boolean generateScanner(File packageDir) throws SilentExit
   {
     if (scannerUpToDate(packageDir))
     {
@@ -177,11 +178,10 @@ public class ParserGeneratorMojo extends AbstractMojo
       return false;
     }
     getLog().info("Generating scanner to "+packageDir+ "...");
-      
-    Options.setDir(packageDir);
-    jflex.Main.generate(scannerFile);
+
+    Options.setRootDirectory(packageDir);
+    jflex.Main.generate(new String[] { scannerFile.getAbsolutePath() });
     getLog().info("Scanner generated to "+ packageDir + ".");
     return true;
   }
-
 }
