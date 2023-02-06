3f5b969835f2923c3c50cce93e1ab4611aeb822e
Alexander Suter <alexander.suter@axonivy.com>
2023-02-06 07:44:26 +0100

    Format code of image-text-plugin

--- image-text-plugin/src/test/java/ch/ivyteam/ivy/maven/TestImageTextMojo.java
+++ image-text-plugin/src/test/java/ch/ivyteam/ivy/maven/TestImageTextMojo.java
@@ -13,15 +13,14 @@ import org.junit.Test;
 import ch.ivyteam.ivy.maven.ImageTextMojo.Align;
 import ch.ivyteam.ivy.maven.ImageTextMojo.Style;
 
-public class TestImageTextMojo
-{
+public class TestImageTextMojo {
+
   @Rule
   public MojoRule rule = new MojoRule();
 
   @Test
-  public void drawOnImage() throws Exception
-  {
-    File project =  new File("src/test/resources");
+  public void drawOnImage() throws Exception {
+    File project = new File("src/test/resources");
     ImageTextMojo mojo = (ImageTextMojo) rule.lookupConfiguredMojo(project, "write-on-image");
     mojo.sourceImage = new File(project, "splash_empty.bmp");
     mojo.text = "hi maven!";
@@ -37,21 +36,17 @@ public class TestImageTextMojo
     mojo.align = Align.CENTER.name();
     mojo.targetImage = Files.createTempFile("myEditedImage", ".bmp").toFile();
     mojo.execute();
-    
     assertThat(mojo.targetImage).exists();
     assertThat(mojo.targetImage.length())
-      .as("generated file must not be empty.")
-      .isGreaterThan(1);
- 
-    //uncomment me for visual feedback!
-    //showVisualFeedback(mojo.targetImage, 10, TimeUnit.SECONDS);
+            .as("generated file must not be empty.")
+            .isGreaterThan(1);
+    // uncomment me for visual feedback!
+    // showVisualFeedback(mojo.targetImage, 10, TimeUnit.SECONDS);
   }
 
   @SuppressWarnings("all")
-  private void showVisualFeedback(File targetImage, int amount, TimeUnit unit) throws InterruptedException
-  {
+  private void showVisualFeedback(File targetImage, int amount, TimeUnit unit) throws InterruptedException {
     ImageViewer.show(targetImage);
     Thread.sleep(unit.toMillis(amount));
   }
-  
 }
